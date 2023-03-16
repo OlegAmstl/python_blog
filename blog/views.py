80e33b99ec1f20f730db3517c30d6189194d9904
Oleg <amstl@mail.ru>
2023-03-16 16:27:10 +0700

    Создан  view и url для домашней странице.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class HomeView(ListView):
+    """
+    Отобрпжение домашней страницы.
+    """
+
+    model = Post
+    template_name = 'blog/index.html'
