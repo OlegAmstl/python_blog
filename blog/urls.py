5d5173224cbc500b0fb89b0fbfe45285123b4569
Oleg <amstl@mail.ru>
2023-03-28 18:27:47 +0700

    Добавлены view и url для строки поиска.

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import HomeView, post_single, TagListView
+from .views import HomeView, post_single, TagListView, PostSearchView
 
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('search/', PostSearchView.as_view(), name='post_search'),
     path('<slug:post_slug>/', post_single, name='post_single'),
     path('tag/<slug:tag>/', TagListView.as_view(), name='post_by_tag')
 ]
